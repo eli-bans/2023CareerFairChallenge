3b0e084b897e4fcdc89b68d65e3fd7221a828afc
eli-bans <114438934+eli-bans@users.noreply.github.com>
2023-03-12 17:36:35 +0000

    Added a few more changes and a docx. file

--- FindSafePolygon.py
+++ FindSafePolygon.py
@@ -2,18 +2,18 @@ import csv
 import sys
 
 
-def calculate_area(p1, p2, p3):
+def calculate_area(point1, point2, point3):
     """
     Calculates the area of a triangle given its three vertices.
     """
-    return abs((p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1])) / 2)
+    return abs((point1[0] * (point2[1] - point3[1]) + point2[0] * (point3[1] - point1[1]) + point3[0] * (point1[1] - point2[1])) / 2)
 
 
-def is_convex(p1, p2, p3):
+def is_convex(point1, point2, point3):
     """
     Checks if three points form a convex polygon.
     """
-    cross_product = (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])
+    cross_product = (point2[0] - point1[0]) * (point3[1] - point1[1]) - (point3[0] - point1[0]) * (point2[1] - p1[1])
     return cross_product > 0
 
 
@@ -29,7 +29,7 @@ def find_safe_polygon(filename):
     for i in range(len(points)):
         for j in range(i + 1, len(points)):
             for k in range(j + 1, len(points)):
-                area = calculateArea(points[i], points[j], points[k])
+                area = calculate_area(points[i], points[j], points[k])
                 if area > max_area:
                     max_area = area
                     max_points = [points[i], points[j], points[k]]
@@ -39,16 +39,8 @@ def find_safe_polygon(filename):
     
     """
 
-    # This is done by checking all possible combinations of three points in the input list 'points',
-    # except for the current max_points list. If a combination of three points forms a convex polygon,
-    # the area of the polygon is calculated by calling calculateArea function. If the sum of the area of
-    # this polygon and the combined area of all other polygons formed by adding a point to it
-    # is greater than the current max_area, then the new set of points is assigned as the new max_points.
-    # The combined area is calculated by iterating over all points and checking if it is inside the
-    # polygon formed by the three points. If it is inside, the area of the triangle formed by the three
-    # points and this point is calculated and added to the combined_area variable.
-    for i in range(len(points)):
-        for j in range(i + 1, len(points)):
+    for i in range(len(points)-2):
+        for j in range(i + 1, len(points)-1):
             for k in range(j + 1, len(points)):
                 # Skip the current max_points list to avoid duplicate calculations
                 if [points[i], points[j], points[k]] == max_points:
@@ -65,9 +57,9 @@ def find_safe_polygon(filename):
                         if is_convex(points[i], points[j], p) and is_convex(points[j], points[k], p) and is_convex(
                                 points[k], points[i], p):
                             # Calculate the area of the triangle formed by the three points and p
-                            combined_area += calculateArea(points[i], points[j], p) + \
-                                             calculateArea(points[j],points[k],p) + \
-                                             calculateArea(points[k], points[i], p)
+                            combined_area += calculate_area(points[i], points[j], p) + \
+                                             calculate_area(points[j],points[k],p) + \
+                                             calculate_area(points[k], points[i], p)
                     # If the total area of the polygon formed by the three points and all additional points
                     # is greater than the current max_area, update max_area and max_points
                     if area + combined_area > max_area:
@@ -82,14 +74,13 @@ def find_safe_polygon(filename):
                                 max_points.append(p)
 
                         # Sort the vertices in increasing order of x-coordinates then y-coordinates
-                        max_points.sort(key=lambda p: (p[0], p[1]))
+                        sorted_points= sorted(max_points,key=lambda p: (p[0], p[1]))
 
     # Write the vertices to a CSV file named safepolygon.csv
     with open('safepolygon.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['X', 'Y'])
-        for point in max_points:
-            writer.writerow(point)
+        writer.writerows(sorted_points)
 
 
 
--- FindSafeTriangle.py
+++ FindSafeTriangle.py
@@ -2,13 +2,13 @@ import csv
 import sys
 
 
-def calculateArea(p1, p2, p3):
+def calculate_area(p1, p2, p3):
     """
     Calculates the area of a triangle given its three vertices using Shoelace formula.
     """
     return abs((p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1])) / 2)
 
-def findPossibleTriangles(points):
+def find_possible_triangles(points):
     """
     Finds all possible triangles that can be formed from the given points.
     """
@@ -24,28 +24,36 @@ def findPossibleTriangles(points):
 def main():
     # to make sure user enters appropriate command
     if len(sys.argv) != 2:
-        print("Usage: python <program name> <filename>")
+        print("Expected <program name> <filename>")
         return
     points = []
     filename = sys.argv[1]
     # read the points from the input file
-    with open(filename, "r") as openedFile:
-        reader = csv.reader(openedFile)
+    with open(filename, "r") as csvfile:
+        reader = csv.reader(csvfile)
         next(reader)  # skip header
         for row in reader:
             x, y = map(float, row)  # convert all strings except the header to float
             points.append((x, y))  # add points as tuples
 
-    maxArea = 0  # initialize maximum area to 0
-    triangles = findPossibleTriangles(points)
+    max_area = 0  # initialize maximum area to 0
+    triangles = find_possible_triangles(points)
     # calculate the area of each triangle and update the maximum area
     for triangle in triangles:
-        currentArea = calculateArea(triangle[0], triangle[1], triangle[2])
-        if currentArea > maxArea:
-            maxArea = currentArea
+        current_area = calculate_area(triangle[0], triangle[1], triangle[2])
+        if current_area > max_area:
+            max_area = current_area
+            coordinates = [triangle[0], triangle[1], triangle[2]]
 
     # print the maximum area of a triangle
-    print("Maximum area of a triangle: ", maxArea)
+    print("Maximum area of a triangle: ", max_area)
+
+    # write the coordinates of the maximum triangle to the file safetriangle.csv
+    sorted_coordinates = sorted(coordinates,key=lambda c: (c[0],c[1]))
+    with open('safetriangle.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['X', 'Y'])
+        writer.writerows(coordinates)
 
 if __name__=="__main__":
     main()
